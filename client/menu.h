180717f012366c28429d4c81757e700416a0a1d1
Marco Messina <darthveder@gmail.com>
2014-12-09 13:02:46 +0100

    Menu for tool actions

---
+++ client/menu.h
@@ -0,0 +1,24 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+
+/**
+ * Menu which displays user choices:
+ * 1 Insert and Send data to Server
+ * 2 Select all data in all DB tables
+ * 3 exit
+ */
+class Menu
+{
+	
+public:
+	void menuChoice( );
+	int selectMenuVoice();
+	
+private:
+	
+	
+};
+
+#endif
