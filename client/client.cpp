f229b98335dac041d8e55b511e5784ff15a1d47b
Marco Messina <darthveder@gmail.com>
2014-12-14 20:50:47 +0100

    Adding doxygen comments on client side

--- client/client.cpp
+++ client/client.cpp
@@ -1,3 +1,11 @@
+/*! \file client.cpp
+	\brief Client code.
+
+	This file contains the Client code for connecting to a remote server and sending or requesting data from a mySQL database.
+
+	Copyright (C) 2014 Marco Messina
+*/
+
 #include <iostream>
 #include <string>
 #include <stdio.h>
@@ -10,7 +18,12 @@
 #include <netdb.h> 
 
 using namespace std;
+using namespace oda;
 
+/**
+* Prints the error message to cerr
+* @param msg string with the error message
+*/
 void error(string msg)
 {
     perror(msg.c_str());
@@ -23,13 +36,16 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    //char buffer[256];
     string buffer;
 
+	// Checking is the command is called with the appropriate sintax:
+	// $ client hostname port
     if (argc < 3) {
        cerr<<"usage: "<<argv[0]<<" hostname port\n";
        exit(0);
     }
+
+	// Setting up the socket
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
@@ -47,22 +63,23 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
+
+	/* Connection with the Server is now established */
     cout<<"Please enter the message: ";
-    //bzero(buffer,256);
-    //fgets(buffer,255,stdin);
     getline(cin,buffer);
     n = write(sockfd,buffer.c_str(),buffer.length());
     if (n < 0) 
          error("ERROR writing to socket");
-    //bzero(buffer,256);
     buffer.clear();
     char string_tmp[256];
-    bzero(string_tmp,256); // need to zeroes the string to avoid junk stuff from read. Why???
+    bzero(string_tmp,256); // need to zeroes the string to avoid junk stuff from read. Still not clear why...
     n = read(sockfd,string_tmp,255);
     buffer = string_tmp;
     if (n < 0) 
          error("ERROR reading from socket");
     cout<<buffer.c_str()<<endl;
+
+	/* Closing the connection */
     close(sockfd);
     return 0;
 }
