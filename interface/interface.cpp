4a0341b27a93ea6a4dcc7c719428a7ee9f93d4f8
marco messina <mm191274@gmail.com>
2015-09-16 09:16:55 +0200

    Adding interface

--- interface/interface.cpp
+++ interface/interface.cpp
@@ -6,93 +6,50 @@
 	Copyright (C) 2014 Marco Messina
 */
 
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-#include "PHTmessage.pb.h"
+#include "interface.h"
+#include "socket.h"
 
-using namespace std;
-using namespace PHT;
-
-/**
-* Prints the error message to cerr
-* @param msg string with the error message
-*/
-
-int connect(PHTmessage &pht_data);
-void error(string msg);
-
-void error(string msg)
-{
-    perror(msg.c_str());
-    exit(0);
-}
+namespace API_ODA {
 
 
-
-int connect(PHTmessage &pht_data)
+int insertNewProposal(Proposals &proposal)
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
-    
-    const int PORT = 3303;
-    string host_name = "localhost";
 
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+    int n;
 
-    // Setting up the socket
-    portno = PORT;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(host_name.c_str());
-    if (server == NULL) 
-    {
-        cerr<<"ERROR, no such host\n";
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+    // setting up the socket and opening the connection
+    Connect connection;
+    if ( connection.openSocket()<0 )
+        cerr<<"ERROR: problems with socket\n";
 
     /* Connection with the Server is now established */
+    // Preparing the message to send
+    PHTmessage *pht_data = new PHTmessage;
+    pht_data->set_type(PHTmessage::DATA);
+    pht_data->set_allocated_proposal(&proposal);
 
     // Serializing to a string the data to send
     string message;
-    if (!pht_data.SerializeToString(&message))
-	    error("ERROR. Can not serialize the message.\n");
+    if (!pht_data->SerializeToString(&message))
+	    cerr<<"ERROR: Can not serialize the message.\n";
 
-    uint32_t dataLength = htonl(message.size()); // convert int to network byte order. 
-    // The size is message.size()+1 because C string terminates with '\0' character, which is missing
-    // in string C++.
+    connection.sendMessage(message);
 
-    // The message e is divided in two parts:
-    // 1) First message sent is the length of the message with the serialization
-    cout<<"Sending "<<message.size()<<" elements\n";
-    n = write(sockfd,&dataLength,sizeof(uint32_t));
-    if (n < 0) 
-        error("ERROR writing to socket the message length");
-    
-    // 2) The actual message is sent. n stores the actual length sent.
-    n = write(sockfd,message.c_str(),dataLength);
-    if (n < 0) 
-        error("ERROR writing to the message socket");
-    
-    /* Closing the connection */
-    close(sockfd);
-    
+    // Closing connection
+    connection.Close();
+
+    //delete pht_data;
+
     return 0;
+}
+
+
+int modifyProposal()
+{
+
+    return 0;
+}
+
+
 }
