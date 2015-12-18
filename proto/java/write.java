b1ac63f889c200618fe82a1adc9ab53f1d5653c1
marco messina <mm191274@gmail.com>
2015-12-18 14:03:20 +0100

    Added query-reply in java

--- proto/java/write.java
+++ proto/java/write.java
@@ -1,58 +1,61 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.OutputStream;
 import java.io.*;
-
 import java.net.*;
 import PHT.*;
 
 class write {
-  // Main function:  Reads the entire address book from a file,
-  //   adds one person based on user input, then writes it back out to the same
-  //   file.
+  // Main function: Send data to the server on localhost:3303 
   public static void main(String[] args) throws Exception {
+      // PHTmessage instance:
     PHTmessageOuterClass.PHTmessage.Builder message = PHTmessageOuterClass.PHTmessage.newBuilder();
 
-
+    // PHTmessage TYPE:
     message.setType(PHTmessageOuterClass.PHTmessage.MessageType.DATA);
-
     System.out.println(message.hasType());
 
+    // Definition of a Proposals with given fields:
     ProposalsOuterClass.Proposals.Builder proposal =  ProposalsOuterClass.Proposals.newBuilder();
     proposal.setAbstract("ahahahahah");
     proposal.setProposalStatus(ProposalStatusOuterClass.ProposalStatus.Draft);
     proposal.setProposalType(ProposalTypeOuterClass.ProposalType.TOO);
 
+    // Adding proposal to the message
     message.addProposal(proposal);
     System.out.println(message.build().toString());
 
+    // Preparing transmission:
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(baos); 
+    PrintStream ps = new PrintStream(baos);
+    // Serialization
     message.build().writeTo(ps);
+
+    // Length of binary string
     byte [] test;
     test = baos.toByteArray();
     System.out.println(test.length);
 
-
+    // TCP/IP connection
     Socket mys;
     mys = new Socket("localhost",3303);
     DataOutputStream msg = new DataOutputStream(mys.getOutputStream());
-    
+    // Data sending:
     msg.writeInt(test.length);
     msg.flush();
     msg.write(test);
     msg.flush();
-    msg.close();
+    //msg.close();
 
+    // reply from the server:
+    /*mys = new Socket("localhost",3303);*/
+    DataInputStream rep = new DataInputStream(mys.getInputStream());
+    int l;
+    l = rep.readInt();
+    System.out.println(l);
+    byte[] reply = new byte[l];
+    rep.readFully(reply,0,l);
+    rep.close();
 
-    // Write the new address book back to disk.
-    //FileOutputStream output = new FileOutputStream("file_name");
-    //message.build().writeTo(output);
-    //output.close();
+    System.out.println(reply.length);
+    PHTmessageOuterClass.PHTmessage m2 = PHTmessageOuterClass.PHTmessage.parseFrom(reply);
+    System.out.println(m2.toString());
   }
 }
