6e8e30fdbfdbbbdb14f71e0b05a2f10985bb9eea
marco messina <mm191274@gmail.com>
2015-12-23 21:43:58 +0100

    Adding Java modifyProposal()

--- java/apioda.java
+++ java/apioda.java
@@ -73,13 +73,13 @@ class apioda {
   }
 
 
-    public static void modifyProposal(ProposalsOuterClass.Proposals proposal, String error_message) throws Exception {
-    // PHTmessage instance:
+    public static void modifyProposal(ProposalsOuterClass.Proposals proposal, StringBuffer error_message) throws Exception {
+            // PHTmessage instance:
     PHTmessageOuterClass.PHTmessage.Builder message = PHTmessageOuterClass.PHTmessage.newBuilder();
-    error_message = "no error";
+    error_message.delete(0,error_message.length()).append("no error");
     // PHTmessage TYPE:
     message.setType(PHTmessageOuterClass.PHTmessage.MessageType.DATA);
-    //System.out.println(message.hasType());
+    //System.out.println("222 "+message.hasType());
 
     // Adding proposal to the message
     message.addProposal(proposal);
@@ -88,6 +88,7 @@ class apioda {
     // Preparing transmission:
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     PrintStream ps = new PrintStream(baos);
+    
     // Serialization
     message.build().writeTo(ps);
 
@@ -98,32 +99,36 @@ class apioda {
 
     // TCP/IP connection
     Socket mys;
-    mys = new Socket("localhost",5200);
+    mys = new Socket("localhost",3303);
     DataOutputStream msg = new DataOutputStream(mys.getOutputStream());
     // Data sending:
+    //System.out.println("Sending message of "+tmp.length);
     msg.writeInt(tmp.length);
     msg.flush();
-    msg.write(tmp);
+    msg.write(tmp,0,tmp.length);
     msg.flush();
-    //msg.close();
+    StringBuffer proposal_id = new StringBuffer("not set");
 
     // reply from the server:
     DataInputStream rep = new DataInputStream(mys.getInputStream());
     int l_rep;
     l_rep = rep.readInt();
-    System.out.println(l_rep);
+    //System.out.println("->"+l_rep);
     byte[] reply = new byte[l_rep];
     rep.readFully(reply,0,l_rep);
-    //rep.close();
 
     //System.out.println(reply.length);
-    //PHTmessageOuterClass.PHTmessage m2 = PHTmessageOuterClass.PHTmessage.parseFrom(reply);
-    //System.out.println(m2.toString());
+    PHTmessageOuterClass.PHTmessage m2;
+    m2 = PHTmessageOuterClass.PHTmessage.parseFrom(reply);
+    //System.out.println("zz "+m2.toString());
+    //System.out.println(m2.getType());
     PHTmessageOuterClass.Answer a;
-    if (message.getType()== PHTmessageOuterClass.PHTmessage.MessageType.ERROR)
+    if (m2.getType()== PHTmessageOuterClass.PHTmessage.MessageType.ERROR)
     {
-        a = message.getAnswer();
-        error_message = a.getAnswer();
+        a = m2.getAnswer();
+        error_message.delete(0,error_message.length()).append(a.getAnswer());
     }
+
+    mys.close();
   }
 }
